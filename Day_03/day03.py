4a4b102c6609a35e5228c1c1cb21a1895c970ec1
PearlsonJob <pearlsonjobj@gmail.com>
2026-07-14 21:39:54 +0530

    Day 3: Input Interface learnt

---
+++ Day_03/day03.py
@@ -0,0 +1,13 @@
+#============================================================================================================
+#Day 3 of becoming the best AI Computational Biologist and the Best Big Data Biologist/Scientist.
+#GENOME X UNIVERSITY
+#Pillar: Programming 
+#Day:03
+#Author: J Pearlson Job
+#============================================================================================================
+name= input("Enter your name: ")
+course= input("Enter your course: ")
+field_of_interest= input("Enter your field of interest: ")
+age= int(input("Enter your age: "))
+print("=========GENOME X PROFILE=========")
+print("Name:", name, "\nCourse:", course, "\nField of Interest:", field_of_interest, "\nAge:", age)
