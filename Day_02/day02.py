4c2b7f7aac613c2699561cf7a4e3c13755933ad4
PearlsonJob <pearlsonjobj@gmail.com>
2026-07-14 21:33:16 +0530

    Create day02.py with personal details
    Added personal information and favorite programming language.

---
+++ Day_02/day02.py
@@ -0,0 +1,9 @@
+#this is the day 2 of my coding to become a master AI Computational Biologist.
+Name="J Pearlson Job"
+Course="M.Sc. Bioinformatics"
+Field_of_Interest="AI in Drug Discovery"
+print(Name)
+print(Course)
+print(Field_of_Interest)    
+Fav_lang="Python"
+print(Fav_lang)
